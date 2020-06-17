d2dfaabd34bdb1e8c3cf8c9575e080b7c5c0145b
Kara Andres <66091350+karaandres@users.noreply.github.com>
2020-06-17 19:18:57 -0400

    Add files via upload

---
+++ multiqc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Peru eDNA metabarcoding — multiqc
+# create working directory and copy files to directory
+
+mkdir /workdir/kja68
+cd /workdir/kja68
+cp /home/kja68/Peru_metabarcoding/raw_sequence_files/*_P_*  /workdir/kja68/
+ls /workdir/kja68/
+
+# Quality control: run multiqc
+fastqc /workdir/kja68/*.gz
+export LC_ALL=en_US.UTF-8
+export PATH=/programs/miniconda3/bin:$PATH
+source activate multiqc
+multiqc /workdir/kja68/
+conda deactivate
