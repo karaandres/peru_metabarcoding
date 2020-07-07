1d2a9435a96167c61040754382dbbfddc7d8bc7b
Kara Andres <66091350+karaandres@users.noreply.github.com>
2020-07-06 20:22:50 -0400

    Update multiqc.sh

--- multiqc.sh
+++ multiqc.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 # Peru eDNA metabarcoding — multiqc
 # create working directory and copy files to directory
 
 mkdir /workdir/kja68
 cd /workdir/kja68
-cp /home/kja68/Peru_metabarcoding/raw_sequence_files/*_P_*  /workdir/kja68/
+cp /home/kja68/Peru_metabarcoding/Trimmed/*fastq.gz /workdir/kja68/
 ls /workdir/kja68/
 
 # Quality control: run multiqc
